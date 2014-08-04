e7ac3dc6640943d311d7d43e5ae42f769213a7ec
Pao <danielpowpao@gmail.com>
2014-08-03 23:02:40 -0700

    Fixing portfolio page.

--- app/scripts/controllers/portfolio.js
+++ app/scripts/controllers/portfolio.js
@@ -3,7 +3,7 @@
 angular.module('paodgithubioApp')
   .controller('PortfolioCtrl', function ($scope, $http) {
     $scope.projects = [];
-    $http.get('../json/projects.json').success(function(data) {
+    $http.get('../json/projects.js').success(function(data) {
       $scope.projects = data;
     });
   });
