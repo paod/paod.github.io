d9b1052e7c88b852de3bd8337bf56de7f47f68f5
Pao <danielpowpao@gmail.com>
2014-08-07 09:14:20 -0700

    Remove  call.

--- app/scripts/controllers/portfolio.js
+++ app/scripts/controllers/portfolio.js
@@ -2,8 +2,28 @@
 
 angular.module('paodgithubioApp')
   .controller('PortfolioCtrl', function ($scope, $http) {
+    /*
     $scope.projects = [];
-    $http.get('../json/projects.js').success(function(data) {
+    $http.get('json/projects.json').success(function(data) {
       $scope.projects = data;
     });
-  });
+  */
+  $scope.projects = [{
+      'name': 'YouInstant',
+      'description': 'YouTube client that fetches YouTube results instantly.',
+      'demolink': 'http://youinstant.herokuapp.com/',
+      'sampleimage': 'images/youinstant.png'
+    },
+    {
+      'name': 'Seize The Diamond',
+      'description': 'Cat n Mouse game where cat is a python, and mouse is a gem.',
+      'sampleimage': 'images/seizethediamond.png'
+    },
+    {
+      'name': 'MyadVenture',
+      'description': 'Built out backend of a KickStarter platform for UCLA internal student entrepreneurs and investors.',
+      'demolink': 'http://myad-venture.com/',
+      'sampleimage': 'images/myadventure.png'
+    }
+  ];
+});
